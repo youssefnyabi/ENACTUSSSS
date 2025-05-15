e8c0753da6f95d6fe66865aca66024ad716f4767
youssefnyabi <youssef.nyabi.2005@gmail.com>
2025-05-15 01:29:21 +0100

    Deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Accueil from './projet/accueil';
 import Membres from './projet/membres';
 import Evenements from './projet/Evenements';
 import Contact from './projet/Contact';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import Rejoindre from './projet/rejoindre';
 import SousAccueil from './projet/sousaccueil';
 import WaveDivider from './WaveDivider';
 function App() {
   return (
     <div className="App">
-     <Router>
+     <BrowserRouter basename="/ENACTUSSSS">
       
       <Navbar />
       <Routes>
@@ -22,7 +22,7 @@ function App() {
         <Route path="/evenements" element={<Evenements />} />
         <Route path="/rejoindre" element={<Rejoindre />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
          <WaveDivider/>
     
       <Contact/>
